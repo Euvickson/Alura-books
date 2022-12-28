ec4d0d1bd87b35630ea497107a4c62b8ee64b6f5
Victor <victor.eugennio@gmail.com>
2022-12-28 09:07:14 -0300

    Elementos de: Cidade, Logradouro, estado e Bairro sendo preenchidos automaticamente ao colocar o CEP no campo devido

--- script.js
+++ script.js
@@ -10,15 +10,29 @@
 //     .finally(mensagem => console.log("Processamento concluído"));
 
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
-        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}}/json/`);
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCepConvertido = await consultaCep.json();
         if(consultaCepConvertido.erro) {
             throw Error('CEP não existente');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCepConvertido.localidade;
+        logradouro.value = consultaCepConvertido.logradouro;
+        estado.value = consultaCepConvertido.uf;
+        bairro.value = consultaCepConvertido.bairro;
+
         console.log(consultaCepConvertido);
         return consultaCepConvertido;
     } catch (erro){
+        console.log(cidade)        
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente</p>`;
         console.log(erro)
     }
 };
@@ -27,3 +41,8 @@ async function buscaEndereco(cep) {
 // let ceps = ['01001001', '01001000'];
 // let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
 // Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+const cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => {
+    buscaEndereco(cep.value)
+})
